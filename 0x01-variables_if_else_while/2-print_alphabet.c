07ee0fe90d6911d551f77de60c876db997a5de6d
favourchidinma <fcakagha9943@gmail.com>
2022-09-09 23:14:43 -0700

    insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-program entry point
+ * Return:0 if no error otherwise non zero
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
