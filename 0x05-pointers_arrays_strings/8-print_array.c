ea33b65016a0943442f6062dd26f07535adff1c9
favourchidinma <fcakagha9943@gmail.com>
2022-09-20 19:23:09 -0700

    8array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_arrat - prints n elements of an array of integers
+ * print_array - prints n elements of an array of integers
  * @a: input array
  * @n: input n elements
  * Return: no return
